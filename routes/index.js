f3fe45242ae2bea1e1acd13dc21c1cac29b19024
Eli Goberdon <goberdon@gmail.com>
2017-08-28 20:18:19 -0400

    Added custom index page and updated default page text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Eli\'s Cool Demo App 👍' });
 });
 
+router.get('/custom/:customText', function(req, res){
+  res.render('index', {title: req.params['customText']})
+})
 module.exports = router;
